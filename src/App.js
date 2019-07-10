2f23f031afd2fb4d761ec57fac8463c190e1b1c1
Anik Barua <onixoni72@gmail.com>
2019-07-11 01:05:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-         <a
-      <h1> Hello from anik </h1> 
+        <p>
+            Hello from anik
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
